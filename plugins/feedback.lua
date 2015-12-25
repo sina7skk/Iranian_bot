9826d8cb05e71cc6bff5b1a7f83b09a72c315215
amirsereen <amirhosseintalebi26@gmail.com>
2015-12-26 00:51:44 +0330

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -19,7 +19,7 @@ local fuses = '!printf user#id' .. msg.from.id
 	else
 
 
-                 local sends0 = send_large_msg('user#id147509695', fuse, ok_cb, false)
+                 local sends0 = send_large_msg('chat#id99975000', fuse, ok_cb, false)
 
  return 'پیام شما با موفقیت ارسال شد'
 
@@ -33,7 +33,7 @@ return {
 
   usage = "!feedback message",
   patterns = {
-    "^!/[Ff]eedback (.*)$"
+    "^![Ff]eedback (.*)$"
 
   },
   run = run
