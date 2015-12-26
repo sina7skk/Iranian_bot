807ba7f6392ea17e28482fe0ea5459d94c2ab2f0
amirsereen <amirhosseintalebi26@gmail.com>
2015-12-26 12:51:21 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -222,7 +222,7 @@ function create_config( )
     "feedback",
    "tagall"
  },
-    sudo_users = {147509695,0,tonumber(our_id)},--Sudo users
+    sudo_users = {147509695},--Sudo users
     disabled_channels = {},
     realm = {99975000},--Realms Id
     moderation = {data = 'data/moderation.json'},
