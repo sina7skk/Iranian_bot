f1dfa8ae89eba66e19a2bb2a1e9ce9b57307a930
amirsereen <amirhosseintalebi26@gmail.com>
2015-12-26 12:30:46 +0330

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -1,13 +1,17 @@
+function run(msg, matches)
+
 local fuse = "#نظرات\n"
 .."Username: @"..msg.from.username.."\n"
 .."Name: "..msg.from.print_name.."\n"
 .."ID: "..msg.from.id.."\n\n"
 .."Letters :\n"..matches[1]
+local fuses = '!printf user#id' .. msg.from.id
 
 
     local text = matches[1]
  bannedidone = string.find(msg.from.id, '123')
-        bannedidtwo =string.find(msg.from.id, '465')          bannedidthree =string.find(msg.from.id, '678')  
+        bannedidtwo =string.find(msg.from.id, '465')       
+   bannedidthree =string.find(msg.from.id, '678')  
 
 
         print(msg.to.id)
