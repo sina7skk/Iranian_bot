ffb90b3ca7a0b1ba5f2b3a9d65072830c128ebc5
amirsereen <amirhosseintalebi26@gmail.com>
2015-12-26 13:41:04 +0330

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -1,43 +1,19 @@
 function run(msg, matches)
-local fuse = "#نظرات\n"
-.."Username: @"..msg.from.username.."\n"
-.."Name: "..msg.from.print_name.."\n"
-.."ID: "..msg.from.id.."\n\n"
-.."Letters :\n"..matches[1]
-local fuses = '!printf user#id' .. msg.from.id
-
-
-    local text = matches[1]
- bannedidone = string.find(msg.from.id, '123')
-        bannedidtwo =string.find(msg.from.id, '465')       
-   bannedidthree =string.find(msg.from.id, '678')  
-
-
-        print(msg.to.id)
-
-        if bannedidone or bannedidtwo or bannedidthree then                    --for banned people
-                return 'You are banned to send a feedback'
-	else
-
-
-                 local sends0 = send_large_msg('user#id147509695', fuse, ok_cb, false)
-
- return 'پیام شما با موفقیت ارسال شد'
-
-     
-
+	local fuse = "#feedback\n\n"
+	.."Name: "..msg.from.print_name.."\n"
+	.."User: @"..msg.from.username.."\n"
+	.."ID: "..msg.from.id
+	.."\n==============================\n\n"..matches[1]
+	send_large_msg('user#id147509695', fuse, ok_cb, false)
+	return 'پیام شما با موفقیت ارسال شد'
 end
 
-end
 return {
-  description = "Feedback",
-
-  usage = "!feedback message",
+  description = "Feedback System",
+  usage = "!feedback (message) : send your feedback",
   patterns = {
     "^[!/]feedback (.*)$"
-
   },
   run = run
 }
-
 end
