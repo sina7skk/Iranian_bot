d4c539314d5958af5cf1a528227a30892d085897
amirsereen <amirhosseintalebi26@gmail.com>
2015-12-30 20:32:05 +0330

    Update antilink.lua

--- plugins/antilink.lua
+++ plugins/antilink.lua
@@ -1,5 +1,5 @@
 local function run(msg)
-    if not is_momod(msg) then
+    if not is_owner(msg) then
         chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
         return '⛔don't send link in this chat⛔'
     end
