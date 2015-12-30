6c4722b5060397d588caa9d51a827fa86826e051
amirsereen <amirhosseintalebi26@gmail.com>
2015-12-30 23:42:29 +0330

    Update antilink.lua

--- plugins/antilink.lua
+++ plugins/antilink.lua
@@ -1,7 +1,7 @@
 local function run(msg)
     if not is_admin(msg) then
         chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-        return 'don't send link here !'
+        return '⛔ don't send link here ⛔'
     end
 end
 
