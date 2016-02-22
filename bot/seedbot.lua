9cadcb03d3b374612ca969f4990f12cf1ba8f9f5
sina7skk <hacker.7sk.33@gmail.com>
2016-02-22 19:35:23 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -225,7 +225,7 @@ function create_config( )
     "feedback",
    "tagall"
  },
-    sudo_users = {147509695},--Sudo users
+    sudo_users = {114934518},--Sudo users
     disabled_channels = {},
     realm = {99975000},--Realms Id
     moderation = {data = 'data/moderation.json'},
